b40a1d4a50732cd598e4d5a0c13c0587e6fe6bcc
Simon <simonpham1294@gmail.com>
2021-09-03 23:51:36 -0300

    disable particlebg for small screen size

--- src/components/ParticleBackground.js
+++ src/components/ParticleBackground.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Particles from "react-tsparticles";
 import particlesConfig from "../config/particleConfig";
 
 const ParticleBackground = () => {
-  const style = {
+  const [smallScreenSize, setSmallScreenSize] = useState(false);
+
+  const resizeDectection = () => {
+    if (window.innerWidth <= 960) {
+      setSmallScreenSize(true);
+    } else {
+      setSmallScreenSize(false);
+    }
+  };
+
+  useEffect(() => {
+    const watchResize = () => {
+      window.addEventListener("resize", resizeDectection);
+    };
+    watchResize();
+  }, []);
+
+  let style = {
     width: "100%",
     height: "100%",
     position: "absolute",
     top: 0,
     left: 0,
+    display: smallScreenSize ? "none" : "block",
   };
+
   return <Particles style={style} params={particlesConfig} />;
 };
 
