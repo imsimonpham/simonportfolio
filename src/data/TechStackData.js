58c60e46abcd89f3d7f23057bb2cf6ea17f403e5
Simon <simonpham1294@gmail.com>
2021-09-25 14:49:19 -0300

    update techstack

--- src/data/TechStackData.js
+++ src/data/TechStackData.js
@@ -2,11 +2,13 @@ export const TechStackData = [
   { cat: "Languages", techList: ["Javascript", "SASS", "CSS3", "HTML5", "C"] },
   {
     cat: "Frontend",
-    techList: ["React", "Redux Toolkit", "React-Bootstrap", "Material-UI", "GSAP"],
-  },
-  {
-    cat: "CMS",
-    techList: ["Wordpress"],
+    techList: [
+      "React",
+      "Redux Toolkit",
+      "React-Bootstrap",
+      "Material-UI",
+      "GSAP",
+    ],
   },
   { cat: "Other Tools", techList: ["Git", "Github", "Netlify", "cPanel"] },
 ];
